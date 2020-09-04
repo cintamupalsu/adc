ebde1e013939308731da24311c31e558230e2a1c
a_maulana <cintamupalsu@hotmail.com>
2020-09-04 10:27:39 +0900

    title

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -4,11 +4,13 @@ class DashboardControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get dashboard_home_url
     assert_response :success
+    assert_select "title", "Home | SBS-ADC ColLabs App"
   end
 
   test "should get help" do
     get dashboard_help_url
     assert_response :success
+    assert_select "title", "Help | SBS-ADC ColLabs App"
   end
 
 end

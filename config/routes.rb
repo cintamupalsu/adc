70e8689d8a8d5bf9881f67fb8105fda5a3a0789d
a_maulana <cintamupalsu@hotmail.com>
2020-09-04 10:11:15 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'dashboard#home'
+  get 'dashboard/home'
+  get 'dashboard/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
